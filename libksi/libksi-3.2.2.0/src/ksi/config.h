0cd213ffbc37948127ab3bc057e668356f8dcf38
friedl <friedl@adiscon.com>
2015-12-04 09:30:35 +0100

    Added packaging files for libksi

---
+++ libksi/libksi-3.2.2.0/src/ksi/config.h
@@ -0,0 +1,84 @@
+/* src/ksi/config.h.  Generated from config.h.in by configure.  */
+/* src/ksi/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Commit id */
+#define COMMIT_ID "aa6d227b11a6220cace9733c70b7ffe3ce644f08"
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `crypto' library (-lcrypto). */
+#define HAVE_LIBCRYPTO 1
+
+/* Define to 1 if you have the `curl' library (-lcurl). */
+#define HAVE_LIBCURL 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Path to the trusted CA certificate directory */
+/* #undef OPENSSL_CA_DIR */
+
+/* Location of the trusted CA certificate bundle file */
+#define OPENSSL_CA_FILE "/etc/pki/ca-trust/extracted/openssl/ca-bundle.trust.crt"
+
+/* Name of package */
+#define PACKAGE "libksi"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "support@guardtime.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libksi"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libksi 3.2.2.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libksi"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.2.2.0"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Location of the unit test xml results. */
+#define UNIT_TEST_OUTPUT_XML "testsuite-xunit.xml"
+
+/* Version number of package */
+#define VERSION "3.2.2.0"
---
+++ libksi/libksi_3.2.2.0-1.dsc
@@ -0,0 +1,36 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA1
+
+Format: 1.0
+Source: libksi
+Binary: libksi-dev, libksi0
+Architecture: any
+Version: 3.2.2.0-1
+Maintainer: Florian Riedl <friedl@adiscon.com>
+Homepage: https://github.com/rsyslog/libksi
+Standards-Version: 3.9.2
+Build-Depends: cdbs, debhelper (>= 7.0.50~), dpkg-dev (>= 1.6.1), autotools-dev (>= 20100122.1), dh-autoreconf, dh-apparmor, pkg-config, hardening-wrapper, libssl-dev, libcurl4-openssl-dev
+Package-List: 
+ libksi-dev deb libdevel extra
+ libksi0 deb libs extra
+Checksums-Sha1: 
+ ffcc891b16a223ac2e38cf2ab3f0db218d4b61fc 543989 libksi_3.2.2.0.orig.tar.gz
+ 332d8bab9b699363a6abc17049a1f71ac48e3470 2430 libksi_3.2.2.0-1.diff.gz
+Checksums-Sha256: 
+ cf63e83e556227f33198e4d9779d6d81f97e1e1034f8231399ebad1dfb89a66d 543989 libksi_3.2.2.0.orig.tar.gz
+ caab6e84cfa9880677d8f2e55b4f22085ec26bbfee9ef6db6488cc6d95fdc0d3 2430 libksi_3.2.2.0-1.diff.gz
+Files: 
+ d753bec46cd3d5c97dd0490d2d3d07df 543989 libksi_3.2.2.0.orig.tar.gz
+ 1b9bc7b4ab688321b895080c36694ecd 2430 libksi_3.2.2.0-1.diff.gz
+
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v1.4.12 (GNU/Linux)
+
+iQEcBAEBAgAGBQJWYCmSAAoJEIlOzxeu8M+OXO8H/3Br+3lSGLiaPSEucAuGZyaH
+bjUWpTKSQVJGotXhbr0Nn7kfPMD7ubvlkDny+jtSr39xJ2Uc1Sr4npoQ5KCnfVos
+JVjN+zXPEh5wiZ32sTzsmuVnCRk2MDvIuBvPusnmvFaOlz0o6kyJF1UP5VZM1UMO
+9+8GvjpP6DdvriDDz8AiT3vU9sh2FqIrmansxVVkdmYUuAJy6iH67gBXiHExMyVi
+4Vjs50yQK3KZh6W9rDtWq57M0o/RQZfq0oZTjgijGjfiaL7UTj7IWjzpVrla3DhH
+uPlA0eA6wFc14UiIeWfcNg41HlKVDTUkQryK+DyW6hxYa8+mcUUYYqier4cTKkw=
+=BRjS
+-----END PGP SIGNATURE-----
